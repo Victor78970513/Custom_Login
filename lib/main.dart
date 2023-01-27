2317044f3123053b4ff606ba97aeccac2654c3e7
Victor78970513 <yer29.chok@gmail.com>
2023-01-27 17:30:27 -0400

    Batman HomeScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'package:custom_login/screens/home_screen.dart';
 
 void main() => runApp(const MyApp());
 
@@ -8,15 +11,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      title: 'Custom Login',
+      home: HomeScreen(),
     );
   }
 }
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Theme(
+        data: ThemeData.light().copyWith(
+          textTheme: GoogleFonts.vidalokaTextTheme(),
+        ),
+        child: Batman_SignUp());
+  }
+}
+
+class Batman_SignUp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF100F0B),
+      body: Stack(
+        children: [
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            child: Image.asset(
+              'assets/batman_background.png',
+              fit: BoxFit.contain,
+            ),
+          ),
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            child: Image.asset(
+              'assets/batman_alone.png',
+              fit: BoxFit.contain,
+            ),
+          ),
+          Positioned(
+            top: MediaQuery.of(context).size.height / 2.5,
+            left: 0,
+            right: 0,
+            child: Image.asset(
+              'assets/batman_logo.png',
+              height: 70,
+              fit: BoxFit.contain,
+            ),
+          ),
+          Positioned(
+              top: MediaQuery.of(context).size.height / 2,
+              left: 0,
+              right: 0,
+              child: Column(
+                children: [
+                  Text(
+                    'WELCOME TO',
+                    // style: TextStyle(
+                    //   color: Colors.white,
+                    //   fontSize: 22,
+                    // ),
+                    style: GoogleFonts.lato(color: Colors.white, fontSize: 22),
+                  ),
+                  Text(
+                    'GOTHAM CITY',
+                    style:
+                        GoogleFonts.vidaloka(color: Colors.white, fontSize: 35),
+                  ),
+                ],
+              )),
+        ],
+      ),
+    );
+  }
+}
